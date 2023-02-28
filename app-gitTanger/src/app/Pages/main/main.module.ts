b3950d0237d32943861f8d61099e0292d0f0d563
Gustavo Daniel Santos <gustavodaniel@tangerino.com.br>
2023-02-28 09:30:37 -0300

    alterações layout

--- app-gitTanger/src/app/Pages/main/main.module.ts
+++ app-gitTanger/src/app/Pages/main/main.module.ts
@@ -2,9 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainComponent } from './main.component';
 import {InputTextModule} from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+
 
 @NgModule({
   declarations: [MainComponent],
-  imports: [CommonModule,InputTextModule],
+  imports: [
+    CommonModule,
+    InputTextModule,
+    ButtonModule
+  ],
 })
 export class MainModule {}
